04503827eddf0a07365cc5db6064e087fae7a9b4
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-10-07 18:02:36 -0300

    pelo amor de deus funciona

--- app/(propostas)/visualizar-propostas/page.tsx
+++ app/(propostas)/visualizar-propostas/page.tsx
@@ -1,26 +1,15 @@
 'use client';
-if (typeof Promise.withResolvers === "undefined") {
-  if (window) {
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window)
     // @ts-expect-error This does not exist outside of polyfill which this is doing
     window.Promise.withResolvers = function () {
-      let resolve, reject
+      let resolve, reject;
       const promise = new Promise((res, rej) => {
-        resolve = res
-        reject = rej
-      })
-      return { promise, resolve, reject }
-    }
-  } else {
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    global.Promise.withResolvers = function () {
-      let resolve, reject
-      const promise = new Promise((res, rej) => {
-        resolve = res
-        reject = rej
-      })
-      return { promise, resolve, reject }
-    }
-  }
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
 }
 import {
   Accordion,
